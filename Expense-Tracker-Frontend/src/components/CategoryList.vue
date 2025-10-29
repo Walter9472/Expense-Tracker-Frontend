ab2b94196e4622bf2b6eeef9b8fea70b98e48364
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-10-29 11:40:58 +0100

    Funktion: Implementierung der CategoryList-Komponente und Aktualisierung der App.vue-Struktur

---
+++ Expense-Tracker-Frontend/src/components/CategoryList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li v-for="cat in props.categories" :key="cat.id">{{ cat.name }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{ categories: { id: number; name: string }[] }>()
+</script>
