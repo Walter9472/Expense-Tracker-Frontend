5412351857e4a1c358d557f786fa221b6a2e2451
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-10-30 22:58:34 +0100

    TransactionList

---
+++ Expense-Tracker-Frontend/src/components/TransactionList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Übersicht</h2>
+        <ul>
+            <li v-for="transaction in transactions" :key="transaction.id">
+                {{ transaction.amount }} € - {{ transaction.type }} - {{ transaction.date }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+
+interface Transaction {
+    id: number
+    title: string
+    amount: number
+    date: string
+    type: string
+    description: string
+
+}
+
+export default {
+    props: {
+        transactions: {
+            type: Array as () => Transaction[],
+            required: true
+        }
+    }
+}
+</script>
