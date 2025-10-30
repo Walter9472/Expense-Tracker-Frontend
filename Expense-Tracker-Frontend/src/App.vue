eceb37be1466b0e41efa271a143b1115be10b5b0
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-10-30 22:55:34 +0100

    Funktion: Hinzufügen der Komponente „Transaktionsliste“ mit dynamischen Transaktionsdaten
    - Transaktionen von der Backend-API mit Axios abrufen
    - Transaktionen in der Komponente „Transaktionsliste“ anzeigen lassen
    - App.vue aktualisiert, um die Komponente „Transaktionsliste“ neben der Komponente „Kategorieliste“ einzufügen zu können

--- Expense-Tracker-Frontend/src/App.vue
+++ Expense-Tracker-Frontend/src/App.vue
@@ -1,28 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
 import CategoryList from './components/CategoryList.vue'
-</script>
+import TransactionList from './components/TransactionList.vue'
+import axios from 'axios'
 
-<template>
-  <div id="app">
-    <CategoryList :categories="categoryArray" />
-  </div>
-</template>
-
-<script lang="ts">
 const categoryArray = ref([
   { id: 1, name: 'Lebensmittel' },
   { id: 2, name: 'Miete' },
   { id: 3, name: 'Freizeit' }
 ])
 
+const transactionArray = ref([]) // Hier initialisieren
+
+onMounted(async () => {
+  const response = await axios.get('http://localhost:8080/et/transactions')
+  transactionArray.value = response.data
+})
+</script>
+
+<template>
+  <div id="app">
+    <TransactionList :transactions="transactionArray" />
+  </div>
+</template>
+
+<script lang="ts">
 export default {
-  components: { CategoryList },
-  data() {
-    return {
-      categoryArray
-    }
-  }
+  components: { CategoryList, TransactionList },
 }
 </script>
