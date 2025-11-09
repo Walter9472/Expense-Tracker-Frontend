9c59e94ca60128be2151ad14ccdab4306f15b276
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-11-09 16:42:00 +0100

    okta config

--- Expense-Tracker-Frontend/src/okta.ts
+++ Expense-Tracker-Frontend/src/okta.ts
@@ -1,6 +1,7 @@
 import { OktaAuth } from '@okta/okta-auth-js'
 
 const { VITE_OKTA_ISSUER, VITE_OKTA_CLIENT_ID } = import.meta.env;
+
 const oktaAuth = new OktaAuth({
   issuer: VITE_OKTA_ISSUER,
   clientId: VITE_OKTA_CLIENT_ID,
