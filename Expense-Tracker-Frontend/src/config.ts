0d2096ac0d38e6fa1c3f99bf5bcb77fff1d18a2c
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-11-10 16:48:29 +0100

    updated config.ts

--- Expense-Tracker-Frontend/src/config.ts
+++ Expense-Tracker-Frontend/src/config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaxa6v8hoZ8zQ5yC697',
     issuer: 'https://integrator-7513753.okta.com/oauth2/default',
-    redirectUri: 'localhost:5173/login/callback',
+    redirectUri: 'http://localhost:5173/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
   }
