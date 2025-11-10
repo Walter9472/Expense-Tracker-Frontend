5b580ce9dd77c83575db7adcbc5b5ccdb82dfb01
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-11-10 18:48:58 +0100

    added Registration

--- Expense-Tracker-Frontend/src/components/Auth/Login.vue
+++ Expense-Tracker-Frontend/src/components/Auth/Login.vue
@@ -1,43 +1,55 @@
-
 <template>
   <div class="login">
     <div id="okta-signin-container"></div>
   </div>
 </template>
 <script setup lang="ts">
-  import OktaSignIn from '@okta/okta-signin-widget'
-  import '@okta/okta-signin-widget/css/okta-sign-in.min.css'
-  import sampleConfig from '@/config'
-  import { onMounted } from 'vue'
-  import { useAuth } from '@okta/okta-vue'
+import OktaSignIn from '@okta/okta-signin-widget'
+import '@okta/okta-signin-widget/css/okta-sign-in.min.css'
+import sampleConfig from '@/config'
+import { onBeforeUnmount, onMounted } from 'vue'
+import { useAuth } from '@okta/okta-vue'
 
-  const auth = useAuth()
+const auth = useAuth()
+let widget: OktaSignIn | null = null
 
-  onMounted(() => {
-    const { issuer, clientId, redirectUri, scopes,baseUrl } = sampleConfig.oidc
+onMounted(() => {
+  const { issuer, clientId, redirectUri, scopes, baseUrl } = sampleConfig.oidc
 
-    const widget = new OktaSignIn({
-      baseUrl,
-      clientId,
-      redirectUri,
-      logo: '/logo.png',
-      i18n: {
-        en: {
-          'primaryauth.title': 'Sign in to Expense Tracker'
-        }
+  const widget = new OktaSignIn({
+    baseUrl,
+    clientId,
+    redirectUri,
+    useInteractionCodeFlow: true,
+    logo: '/logo.png',
+    i18n: {
+      en: {
+        'primaryauth.title': 'Sign in to Expense Tracker',
       },
-      authParams: { issuer,scopes },
-      features: { registration: true,},
-    })
-    widget.showSignInToGetTokens({
+    },
+    authParams: { issuer, scopes },
+    features: { registration: true },
+  })
+  widget
+    .showSignInToGetTokens({
       el: '#okta-signin-container',
-    scopes,
-    }).then(tokens => {
+      scopes,
+    })
+    .then((tokens) => {
       // handle tokens here
       auth.handleLoginRedirect(tokens)
       console.log('Login success', tokens)
-    }).catch(err => { console.error('Login error', err)})
-  })
+    })
+    .catch((err) => {
+      console.error('Login error', err)
+    })
+})
+onBeforeUnmount(() => {
+  if(widget) {
+    widget.remove()
+    widget = null
+  }
+})
 </script>
 
 <style scoped></style>
